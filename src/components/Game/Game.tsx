596cee746ab91544bd36e1d2bd99740600ab111c
Elena Pogodaeva <pogodaeva_e@inbox.ru>
2023-03-02 20:08:57 +0300

    feat: add Game component

---
+++ src/components/Game/Game.tsx
@@ -0,0 +1,15 @@
+import Board from 'components/Board/Board';
+import React, { useState } from 'react';
+import style from './Game.module.scss';
+
+export const Game = () => {
+  const [gameStatus, setGameStatus] = useState('start');
+
+  return (
+    <div className={style.game}>
+      <Board gameStatus={gameStatus} setGameStatus={setGameStatus} />
+    </div>
+  );
+};
+
+export default Game;
